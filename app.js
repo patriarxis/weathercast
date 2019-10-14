5730ad01803e3c591dc1bf9847684735f343f333
Fotios Pechlivanis <fotispechli@gmail.com>
2019-10-14 03:50:19 +0300

    Added five days forecast style and animation
    Five days forecast style and animation plus custom scrollbar and new images.

--- app.js
+++ app.js
@@ -84,6 +84,87 @@ window.addEventListener('load', ()=> {
     }
 
 
+    var dayDetails = document.querySelectorAll(".five-day-forecast ul li")
+    var details = document.querySelectorAll(".details");
+    let delay = 0;
+
+    dayDetails[0].addEventListener('click', function() {
+        if(details[0].classList.contains("show-details")) {
+            delay = 300;
+            details[0].classList.add("animate-details");
+        } else {
+            delay = 0;
+            details[0].classList.remove("animate-details");
+        }
+
+        dayDetails[0].classList.toggle("active-day");
+        
+        setTimeout(function() {
+            details[0].classList.toggle("show-details");
+        }, delay);
+    });
+    dayDetails[1].addEventListener('click', function() {
+        if(details[1].classList.contains("show-details")) {
+            delay = 300;
+            details[1].classList.add("animate-details");
+        } else {
+            delay = 0;
+            details[1].classList.remove("animate-details");
+        }
+
+        dayDetails[1].classList.toggle("active-day");
+        
+        setTimeout(function() {
+            details[1].classList.toggle("show-details");
+        }, delay);
+    });
+    dayDetails[2].addEventListener('click', function() {
+        if(details[2].classList.contains("show-details")) {
+            delay = 300;
+            details[2].classList.add("animate-details");
+        } else {
+            delay = 0;
+            details[2].classList.remove("animate-details");
+        }
+
+        dayDetails[2].classList.toggle("active-day");
+        
+        setTimeout(function() {
+            details[2].classList.toggle("show-details");
+        }, delay);
+    });
+    dayDetails[3].addEventListener('click', function() {
+        if(details[3].classList.contains("show-details")) {
+            delay = 300;
+            details[3].classList.add("animate-details");
+        } else {
+            delay = 0;
+            details[3].classList.remove("animate-details");
+        }
+
+        dayDetails[3].classList.toggle("active-day");
+        
+        setTimeout(function() {
+            details[3].classList.toggle("show-details");
+        }, delay);
+    });
+    dayDetails[4].addEventListener('click', function() {
+        if(details[4].classList.contains("show-details")) {
+            delay = 300;
+            details[4].classList.add("animate-details");
+        } else {
+            delay = 0;
+            details[4].classList.remove("animate-details");
+        }
+
+        dayDetails[4].classList.toggle("active-day");
+        
+        setTimeout(function() {
+            details[4].classList.toggle("show-details");
+        }, delay);
+    });
+    
+
     
     /* Settings dropdown menu */
     var dropdown = document.querySelector(".dropdown");
@@ -101,7 +182,6 @@ window.addEventListener('load', ()=> {
         dropdown.classList.remove("show-grid");
         closeDropdown.classList.remove("show-block");
     });
-
 
 
 
